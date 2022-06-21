ca0361d8ff1ef45b028ddcf8a1aa1adbee4ef227
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-06-21 10:21:16 +0000

    Navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element = {<Home/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
