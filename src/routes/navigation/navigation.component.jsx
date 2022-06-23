421d99ae1e96acfc4cbe24a7f5c29174267cea88
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-06-23 15:27:16 +0000

    User Context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,15 @@
+import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+
+import { UserContext } from '../../contexts/user.context';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  console.log(currentUser);
+
   return (
     <>
       <div className="navigation">
@@ -14,7 +21,7 @@ const Navigation = () => {
             SHOP
           </Link>
           <Link className="nav-link" to="/auth">
-            SIGN IN 
+            SIGN IN
           </Link>
         </div>
       </div>
