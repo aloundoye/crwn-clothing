ad25964ea200057d4cd03ffe12bb375db85b1746
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-07-04 14:18:01 +0000

    add to cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,23 @@
-import './cart-dropdown.styles.scss'
-import Button  from '../button/button.component';
+import { useContext } from 'react';
 
-const CartDropdown = () =>{
-    return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items"/>
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    )
-}
+import { CartContext } from '../../contexts/cart.context';
+
+import './cart-dropdown.styles.scss';
+
+import CartItem from '../cart-item/cart-item.component';
+import Button from '../button/button.component';
+
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((cartItem)=> <CartItem key={cartItem.id} cartItem={cartItem} />)}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
