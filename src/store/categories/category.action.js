99e6a063edca0b641b10654ba64d21b1b7b108f2
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-07-29 13:19:48 +0000

    Update category.action.js

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,5 @@
 import { createAction } from '../../utils/reducer/reducer.utils';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
 export const setCategories = (categories) =>
   createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
